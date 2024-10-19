3a5ef549da0c18d6306dfb10f24c70757f2893ad
ramohin1236 <mohinr26@gmail.com>
2024-10-19 10:37:37 -0700

    image upload porblem

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@ function showProjects(projects) {
     projects?.map((project) => {
         projectHTML += `
         <div class="box tilt" >
-      <img draggable="false" src="/${project.image}.png" alt="project" />
+      <img draggable="false" src="./${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
