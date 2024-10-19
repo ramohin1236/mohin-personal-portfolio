d53f479c985f5307ac01468c361711e85fafd8cc
ramohin1236 <mohinr26@gmail.com>
2024-10-19 10:26:26 -0700

    image in root folder

--- projects/script.js
+++ projects/script.js
@@ -30,48 +30,48 @@ function getProjects() {
 }
 
 
-function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
-    });
-    projectsContainer.innerHTML = projectsHTML;
+// function showProjects(projects) {
+//     let projectsContainer = document.querySelector(".work .box-container");
+//     let projectsHTML = "";
+//     projects.forEach(project => {
+//         projectsHTML += `
+//         <div class="grid-item ${project.category}">
+//         <div class="box tilt" style="width: 380px; margin: 1rem">
+//       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+//       <div class="content">
+//         <div class="tag">
+//         <h3>${project.name}</h3>
+//         </div>
+//         <div class="desc">
+//           <p>${project.desc}</p>
+//           <div class="btns">
+//             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+//             <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>`
+//     });
+//     projectsContainer.innerHTML = projectsHTML;
 
-    // isotope filter products
-    var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
-    });
+//     // isotope filter products
+//     var $grid = $('.box-container').isotope({
+//         itemSelector: '.grid-item',
+//         layoutMode: 'fitRows',
+//         masonry: {
+//             columnWidth: 200
+//         }
+//     });
 
-    // filter items on button click
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-}
+//     // filter items on button click
+//     $('.button-group').on('click', 'button', function () {
+//         $('.button-group').find('.is-checked').removeClass('is-checked');
+//         $(this).addClass('is-checked');
+//         var filterValue = $(this).attr('data-filter');
+//         $grid.isotope({ filter: filterValue });
+//     });
+// }
 
 getProjects().then(data => {
     showProjects(data);
